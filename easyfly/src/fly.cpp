513998b4cb6d7ba77400282d9822ab2989b90eba
Walt Yeh <yexin5120209375@sjtu.edu.cn>
2016-10-04 21:47:32 +0800

    2 joysticks available in 2 groups

--- easyfly/src/fly.cpp
+++ easyfly/src/fly.cpp
@@ -18,10 +18,11 @@ public:
   void run(double frequency);
   std::string uri;
 private:
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+  void joyCallback0(const sensor_msgs::Joy::ConstPtr& joy0);
+  void joyCallback1(const sensor_msgs::Joy::ConstPtr& joy1);
   ros::NodeHandle nh;
-  ros::Subscriber joy_sub;
-  
+  ros::Subscriber joy_sub0;
+  ros::Subscriber joy_sub1;
 //  int init_scan(int argc, char **argv);
 //  int connect_get_param(int argc, char **argv);
   void iteration(const ros::TimerEvent& e);
@@ -41,7 +42,8 @@ cf(g_uri)
 //  Crazyflie cf(uri);
 //  cf(uri);
   cf.requestParamToc();
-  joy_sub = nh.subscribe<sensor_msgs::Joy>("/joy",5,&fly212::joyCallback,this);
+  joy_sub0 = nh.subscribe<sensor_msgs::Joy>("/joygroup0/joy",5,&fly212::joyCallback0,this);
+  joy_sub1 = nh.subscribe<sensor_msgs::Joy>("/joygroup1/joy",5,&fly212::joyCallback1,this);
 }
 void fly212::run(double frequency)
 {
@@ -52,14 +54,21 @@ void fly212::run(double frequency)
 void fly212::iteration(const ros::TimerEvent& e)
 {
   float dt = e.current_real.toSec() - e.last_real.toSec();
-  printf("pitch: %f\n", pitch_sp[0]);
-  printf("roll: %f\n", roll_sp[0]);
-  printf("yaw: %f\n", yaw_sp[0]);
-  printf("thrust: %f\n", thrust_sp[0]);
+  printf("pitch: %f\t", pitch_sp[0]);
+  printf("pitch: %f\n", pitch_sp[1]);
+
+  printf("roll: %f\t", roll_sp[0]);
+  printf("roll: %f\n", roll_sp[1]);
+
+  printf("yaw: %f\t", yaw_sp[0]);
+  printf("yaw: %f\n", yaw_sp[1]);
+
+  printf("thrust: %f\t", thrust_sp[0]);
+  printf("thrust: %f\n", thrust_sp[1]);
 
   cf.sendSetpoint(-roll_sp[0]*20, -pitch_sp[0]*20, -yaw_sp[0]*50, thrust_sp[0]*40000);
 }
-void fly212::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+void fly212::joyCallback0(const sensor_msgs::Joy::ConstPtr& joy)
 {
   pitch_sp[0] = joy->axes[5];
   roll_sp[0] = joy->axes[2];
@@ -67,6 +76,15 @@ void fly212::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
   thrust_sp[0] = joy->axes[1];
   if(thrust_sp[0]<0)
     thrust_sp[0]=0;
+}
+void fly212::joyCallback1(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  pitch_sp[1] = joy->axes[5];
+  roll_sp[1] = joy->axes[2];
+  yaw_sp[1] = joy->axes[0];
+  thrust_sp[1] = joy->axes[1];
+  if(thrust_sp[1]<0)
+    thrust_sp[1]=0;
 }
 int init_scan(int argc, char **argv)
 {
---
+++ easyfly/src/joysticks2.cpp
@@ -0,0 +1,103 @@
+#include "ros/ros.h"
+#include <iostream>
+#include <sensor_msgs/Joy.h>
+#include <boost/program_options.hpp>
+#include <crazyflie_cpp/Crazyradio.h>
+#include <crazyflie_cpp/Crazyflie.h>
+/******************
+
+MUST RUN IN ROOT!!!!!!!!!!!!!!
+
+
+*************************/
+std::string g_uri;
+class fly212
+{
+public:
+  fly212(int argc, char **argv);
+  void run(double frequency);
+  std::string uri;
+private:
+  void joyCallback0(const sensor_msgs::Joy::ConstPtr& joy0);
+  void joyCallback1(const sensor_msgs::Joy::ConstPtr& joy1);
+  ros::NodeHandle nh;
+
+  ros::Subscriber joy_sub0;
+  ros::Subscriber joy_sub1;
+//  int init_scan(int argc, char **argv);
+//  int connect_get_param(int argc, char **argv);
+  void iteration(const ros::TimerEvent& e);
+  
+  float pitch_sp[2];
+  float roll_sp[2];
+  float yaw_sp[2];
+  float thrust_sp[2];
+//  Crazyflie cf;
+
+};
+
+fly212::fly212(int argc, char **argv)//:
+//cf(g_uri)
+{
+//  uri=g_uri;
+//  Crazyflie cf(uri);
+//  cf(uri);
+//  cf.requestParamToc();
+  joy_sub0 = nh.subscribe<sensor_msgs::Joy>("/joygroup0/joy",5,&fly212::joyCallback0,this);
+  joy_sub1 = nh.subscribe<sensor_msgs::Joy>("/joygroup1/joy",5,&fly212::joyCallback1,this);
+}
+void fly212::run(double frequency)
+{
+  ros::NodeHandle node;
+  ros::Timer timer = node.createTimer(ros::Duration(1.0/frequency), &fly212::iteration, this);
+  ros::spin();
+}
+void fly212::iteration(const ros::TimerEvent& e)
+{
+  float dt = e.current_real.toSec() - e.last_real.toSec();
+  printf("pitch: %f\t", pitch_sp[0]);
+  printf("pitch: %f\n", pitch_sp[1]);
+
+  printf("roll: %f\t", roll_sp[0]);
+  printf("roll: %f\n", roll_sp[1]);
+
+  printf("yaw: %f\t", yaw_sp[0]);
+  printf("yaw: %f\n", yaw_sp[1]);
+
+  printf("thrust: %f\t", thrust_sp[0]);
+  printf("thrust: %f\n", thrust_sp[1]);
+
+//  cf.sendSetpoint(-roll_sp[0]*20, -pitch_sp[0]*20, -yaw_sp[0]*50, thrust_sp[0]*40000);
+}
+void fly212::joyCallback0(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  pitch_sp[0] = joy->axes[5];
+  roll_sp[0] = joy->axes[2];
+  yaw_sp[0] = joy->axes[0];
+  thrust_sp[0] = joy->axes[1];
+  if(thrust_sp[0]<0)
+    thrust_sp[0]=0;
+}
+void fly212::joyCallback1(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  pitch_sp[1] = joy->axes[5];
+  roll_sp[1] = joy->axes[2];
+  yaw_sp[1] = joy->axes[0];
+  thrust_sp[1] = joy->axes[1];
+  if(thrust_sp[1]<0)
+    thrust_sp[1]=0;
+}
+
+int main(int argc, char **argv)
+{
+//  int ret = init_scan(argc, argv);
+  ros::init(argc, argv, "joysticks2");
+  
+  fly212 Fly212(argc, argv);
+  Fly212.run(10);
+
+
+  return 0;
+
+
+}
