a2120b659ce9683ac413c3c8bc53f49e9ad0c6c1
Walt Yeh <yexin5120209375@sjtu.edu.cn>
2016-10-04 21:12:15 +0800

    class structuralized

--- easyfly/src/fly.cpp
+++ easyfly/src/fly.cpp
@@ -10,18 +10,63 @@ MUST RUN IN ROOT!!!!!!!!!!!!!!
 
 
 *************************/
-float pitch_sp=0;
-float roll_sp=0;
-float yaw_sp=0;
-float thrust_sp=0;
-
-std::string uri;
-void sticksCallback(const sensor_msgs::Joy msg)
+std::string g_uri;
+class fly212
 {
-  pitch_sp = msg.axes[5];
-  roll_sp = msg.axes[2];
-  yaw_sp = msg.axes[0];
-  thrust_sp = msg.axes[1];
+public:
+  fly212(int argc, char **argv);
+  void run(double frequency);
+  std::string uri;
+private:
+  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+  ros::NodeHandle nh;
+  ros::Subscriber joy_sub;
+  
+//  int init_scan(int argc, char **argv);
+//  int connect_get_param(int argc, char **argv);
+  void iteration(const ros::TimerEvent& e);
+  
+  float pitch_sp[2];
+  float roll_sp[2];
+  float yaw_sp[2];
+  float thrust_sp[2];
+  Crazyflie cf;
+
+};
+
+fly212::fly212(int argc, char **argv):
+cf(g_uri)
+{
+  uri=g_uri;
+//  Crazyflie cf(uri);
+//  cf(uri);
+  cf.requestParamToc();
+  joy_sub = nh.subscribe<sensor_msgs::Joy>("/joy",5,&fly212::joyCallback,this);
+}
+void fly212::run(double frequency)
+{
+  ros::NodeHandle node;
+  ros::Timer timer = node.createTimer(ros::Duration(1.0/frequency), &fly212::iteration, this);
+  ros::spin();
+}
+void fly212::iteration(const ros::TimerEvent& e)
+{
+  float dt = e.current_real.toSec() - e.last_real.toSec();
+  printf("pitch: %f\n", pitch_sp[0]);
+  printf("roll: %f\n", roll_sp[0]);
+  printf("yaw: %f\n", yaw_sp[0]);
+  printf("thrust: %f\n", thrust_sp[0]);
+
+  cf.sendSetpoint(-roll_sp[0]*20, -pitch_sp[0]*20, -yaw_sp[0]*50, thrust_sp[0]*40000);
+}
+void fly212::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  pitch_sp[0] = joy->axes[5];
+  roll_sp[0] = joy->axes[2];
+  yaw_sp[0] = joy->axes[0];
+  thrust_sp[0] = joy->axes[1];
+  if(thrust_sp[0]<0)
+    thrust_sp[0]=0;
 }
 int init_scan(int argc, char **argv)
 {
@@ -113,7 +158,7 @@ int connect_get_param(int argc, char **argv)
   po::options_description desc("Allowed options");
   desc.add_options()
     ("help", "produce help message")
-    ("uri", po::value<std::string>(&uri)->default_value(defaultUri), "unique ressource identifier")
+    ("uri", po::value<std::string>(&g_uri)->default_value(defaultUri), "unique ressource identifier")
   ;
 
   try//without which the uri will not be valid
@@ -147,30 +192,10 @@ int connect_get_param(int argc, char **argv)
 int main(int argc, char **argv)
 {
 //  int ret = init_scan(argc, argv);
-  int ret2=connect_get_param(argc, argv);
-  Crazyflie cf(uri);
-  cf.requestParamToc();
-
-
-
-
   ros::init(argc, argv, "fly");
-  ros::NodeHandle n;
-  ros::Subscriber commands_sub = n.subscribe("/joy",5,sticksCallback);
-  ros::Rate loop_rate(50);
-
-  int count = 0;
-  while (ros::ok()){
-    printf("pitch: %f\n", pitch_sp);
-    printf("roll: %f\n", roll_sp);
-    printf("yaw: %f\n", yaw_sp);
-    printf("thrust: %f\n", thrust_sp);
-
-    cf.sendSetpoint(-roll_sp*20, -pitch_sp*20, -yaw_sp*50, thrust_sp*40000);
-    ros::spinOnce();
-    loop_rate.sleep();
-    ++count;
-  }
+  int ret2 = connect_get_param(argc, argv);
+  fly212 Fly212(argc, argv);
+  Fly212.run(50);
 
 
   return 0;
